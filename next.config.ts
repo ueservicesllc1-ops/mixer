e51ef833a1f9659e0d16066b06ef33f02e2d916b
Your Name <you@example.com>
2025-09-18 00:12:59 +0000

    ode_modules/handlebars/lib/index.js ./node_modules/dotprompt/dist/index.

--- next.config.ts
+++ next.config.ts
@@ -38,6 +38,12 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...config.externals, 'handlebars'];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
